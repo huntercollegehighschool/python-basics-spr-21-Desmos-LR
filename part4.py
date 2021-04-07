71d7f8bb8657116d4a48dd99522498c5886f8d03
Logan Reich <>
2021-04-07 20:27:44 +0000

    I wrote out the exercise as requested in the instructions, and I added in some error-handling in part 4

--- part4.py
+++ part4.py
@@ -12,5 +12,16 @@ Enter the height: 3
 The area of the triangle is 12.0
 
 '''
-
 #start writing your code below
+print("Hi, welcome to triangle-area finder.")
+try:
+  side1=int(input("Enter the value of one of the triangle's legs: "))
+except ValueError:
+  side1=int(input("Re-enter the value of one of the triangle's legs, this time using numerals(e.g. 0,1,2,3...): "))
+try:
+  side2=int(input("Enter the value of the triangle's other leg: "))
+except ValueError:
+  side2=int(input("Re-enter the value of the triangle's other leg, this time using numerals(e.g. 0,1,2,3...): "))
+print("The area of your triangle is",side1*side2/2)
+print("Hope you enjoyed triangle-area finder!")
+
